2e0aec1e8336242e8017bfb29c38ae759de90a29
numangit <abdulnumanjob@gmail.com>
2023-03-05 17:03:01 +0600

    env file added

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
