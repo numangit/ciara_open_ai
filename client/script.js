4ec1809816aaad9d3a52157d3e0cee54d9197251
numangit <abdulnumanjob@gmail.com>
2023-03-05 07:27:43 +0600

    chat thinking loader added

--- client/script.js
+++ client/script.js
@@ -4,3 +4,19 @@ import user from './assets/user.svg';
 //DOM Elements
 const formEl = document.querySelector('form');
 const chatContainerEl = document.querySelector('#chat-container');
+
+let loadInterval;
+
+//function to set chat loader (dot animation)
+const loader = (element) => {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === "....") {
+      element.textContent = '';
+    };
+
+  }, 300);
+};
