a2daac2e2e4f5a3ffd7867769db47af5d95f219f
numangit <abdulnumanjob@gmail.com>
2023-03-05 07:20:16 +0600

    dom elements picked

---
+++ client/script.js
@@ -0,0 +1,6 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+//DOM Elements
+const formEl = document.querySelector('form');
+const chatContainerEl = document.querySelector('#chat-container');
